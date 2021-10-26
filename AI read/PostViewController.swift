74ef72c886f07766316de69a5a4b25a51a04e9c1
MK751231 <koshikawa-m@nespe.com>
2021-10-26 10:38:28 +0900

    リリースバージョン

--- AI read/PostViewController.swift
+++ AI read/PostViewController.swift
@@ -10,7 +10,7 @@ import Vision
 import VisionKit	
 import Alamofire
 
-class PostViewController: UIViewController {
+class PostViewController: UIViewController, UINavigationControllerDelegate, URLSessionDelegate {
 
     @IBOutlet weak var imageView: UIImageView!
     @IBOutlet weak var textView: UITextView!
@@ -18,7 +18,6 @@ class PostViewController: UIViewController {
     var requests = [VNRequest]()
     var resultingText = ""
     var activityIndicator: UIActivityIndicatorView!
-
 
     var text: String? = ""
     var image: UIImage!
@@ -28,6 +27,62 @@ class PostViewController: UIViewController {
     }
     
     @IBAction func handlePostButton(_ sender: Any) {
+        
+        var alertTextField: UITextField?
+
+        let alert = UIAlertController(
+            title: "ファイル転送",
+            message: "物件CDを入力してください",
+            preferredStyle: UIAlertController.Style.alert)
+        alert.addTextField(
+            configurationHandler: {(textField: UITextField!) in
+                alertTextField = textField
+                textField.text = ""
+                // textField.placeholder = "Mike"
+                // textField.isSecureTextEntry = true
+        })
+        alert.addAction(
+            UIAlertAction(
+                title: "キャンセル",
+                style: UIAlertAction.Style.cancel,
+                handler: nil))
+        alert.addAction(
+            UIAlertAction(
+                title: "転送",
+                style: UIAlertAction.Style.default) { _ in
+                if let text = alertTextField?.text {
+                    let imageJpgData = self.imageView.image!.jpegData(compressionQuality: 1)
+                    let textData = self.textView.text.data(using: .utf8)
+                    let propertyCode = text.data(using: .utf8)
+                    // let propertyCode = self.textField.text!.data(using: .utf8)
+
+                    AF.upload(
+                        multipartFormData: { multipartFormData in
+                            multipartFormData.append(imageJpgData!, withName: "upload_img_jpg" , fileName: "test01.jpg", mimeType: "image/jpeg")
+                            multipartFormData.append(textData!, withName: "upload_txt" , fileName: "test01.txt", mimeType: "text/plain")
+                            multipartFormData.append(propertyCode!, withName: "upload_code" , fileName: "code.txt", mimeType: "text/plain")
+                        },
+                        // to: "http://192.168.97.160/test2.php", method: .post
+                        to: "https://sf.489501.jp/kotei/d2b/test2.php", method: .post
+                        )
+                        .response { resp in
+                            switch resp.result {
+                                case .success:
+                                    print("response is:", resp)
+                                    DispatchQueue.main.async {
+                                        self.dismiss(animated: true)
+                                        // 正常終了メッセージ表示実装
+                                        
+                                    }
+                                case let .failure(error):
+                                    print(error)
+                                        // 異常終了メッセージ表示実装
+                            }
+                        }
+                }
+            }
+        )
+        self.present(alert, animated: true, completion: nil)
     }
     
     override func viewDidLoad() {
